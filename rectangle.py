c8e9e1a3af9d03cafb30293e8d090456ee0a8059
Prathmesh Wadiya <pratham.wadiya0@gmail.com>
2024-06-17 12:15:49 +0530

    Fixed bug in Perimeter Function

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,15 @@
 def perimeter_rectangle(length,breadth):
-    p=3*(length+breadth)
+    p=2*(length+breadth)
     return p
    
 def area_rectangle(length,breadth):
     a=length*breadth
     return a
     
+l=int(input('Enter the Length :'))
+b=int(input('Enter the breadth :'))
+
+print(perimeter_rectangle(l,b))
+print(area_rectangle(l,b))
+
 
