ddb48ca83af4ccc1555ae7eaae0a58bc6ec8ac26
Prathmesh Wadiya <pratham.wadiya0@gmail.com>
2024-06-17 12:06:17 +0530

    Added code for user input

--- rectangle.py
+++ rectangle.py
@@ -6,4 +6,10 @@ def area_rectangle(length,breadth):
     a=length*breadth
     return a
     
+l=int(input('Enter the Length :'))
+b=int(input('Enter the breadth :'))
+
+print(perimeter_rectangle(l,b))
+print(area_rectangle(l,b))
+
 
