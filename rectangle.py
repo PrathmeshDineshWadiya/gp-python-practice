39600661769f6e7f0a6297a2eba063a5bcc8afd8
Prathmesh Wadiya <pratham.wadiya0@gmail.com>
2024-06-17 12:15:49 +0530

    Added support for default parameter with a breadth value as 2

--- rectangle.py
+++ rectangle.py
@@ -1,8 +1,8 @@
-def perimeter_rectangle(length,breadth):
+def perimeter_Rectangle(length,breadth):
     p=2*(length+breadth)
     return p
    
-def area_rectangle(length,breadth):
+def area_Rectangle(length,breadth=2):
     a=length*breadth
     return a
     
